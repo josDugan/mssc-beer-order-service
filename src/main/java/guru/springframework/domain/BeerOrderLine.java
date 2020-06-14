d542afe55f58fec6fb03d1f26961782ea70aa7c0
josDugan <josephdug@gmail.com>
2020-06-14 00:05:47 -0400

    Add upc to beer order line. Closes #3

--- src/main/java/guru/springframework/domain/BeerOrderLine.java
+++ src/main/java/guru/springframework/domain/BeerOrderLine.java
@@ -16,19 +16,22 @@ import java.util.UUID;
 @Entity
 public class BeerOrderLine extends BaseEntity {
 
+    @Builder
+    public BeerOrderLine(UUID id, String upc, Long version, Timestamp createdDate, Timestamp lastModifiedDate, BeerOrder beerOrder, UUID beerId, Integer orderQuantity, Integer quantityAllocated) {
+        super(id, version, createdDate, lastModifiedDate);
+        this.beerOrder = beerOrder;
+        this.beerId = beerId;
+        this.upc = upc;
+        this.orderQuantity = orderQuantity;
+        this.quantityAllocated = quantityAllocated;
+    }
+
     @ManyToOne
     private BeerOrder beerOrder;
 
     private UUID beerId;
+    private String upc;
     private Integer orderQuantity = 0;
     private Integer quantityAllocated = 0;
 
-    @Builder
-    public BeerOrderLine(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate, BeerOrder beerOrder, UUID beerId, Integer orderQuantity, Integer quantityAllocated) {
-        super(id, version, createdDate, lastModifiedDate);
-        this.beerOrder = beerOrder;
-        this.beerId = beerId;
-        this.orderQuantity = orderQuantity;
-        this.quantityAllocated = quantityAllocated;
-    }
 }
