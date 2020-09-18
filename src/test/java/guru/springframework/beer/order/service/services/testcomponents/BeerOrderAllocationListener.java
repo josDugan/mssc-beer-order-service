61429fc1b3fe26834ac56522c2cba22aab4973f8
josDugan <josephdug@gmail.com>
2020-09-18 16:02:56 -0400

    Complete happy path integration test. Closes #19.

---
+++ src/test/java/guru/springframework/beer/order/service/services/testcomponents/BeerOrderAllocationListener.java
@@ -0,0 +1,37 @@
+package guru.springframework.beer.order.service.services.testcomponents;
+
+import guru.springframework.beer.order.service.config.JMSConfig;
+import guru.springframework.brewery.events.AllocateBeerOrderRequest;
+import guru.springframework.brewery.events.AllocateBeerOrderResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class BeerOrderAllocationListener {
+
+    private final JmsTemplate jmsTemplate;
+
+    @JmsListener(destination = JMSConfig.ALLOCATE_ORDER_QUEUE)
+    public void listen(Message msg) {
+        AllocateBeerOrderRequest request = (AllocateBeerOrderRequest) msg.getPayload();
+
+        request.getBeerOrderDto().getBeerOrderLines().forEach(beerOrderLineDto -> {
+            beerOrderLineDto.setQuantityAllocated(beerOrderLineDto.getOrderQuantity());
+        });
+
+        log.debug("test component beer order allocation listener ###########################");
+
+        jmsTemplate.convertAndSend(JMSConfig.ALLOCATE_ORDER_RESULT_QUEUE,
+                AllocateBeerOrderResponse.builder()
+                        .allocationError(false)
+                        .pendingInventory(false)
+                        .beerOrderDto(request.getBeerOrderDto())
+                        .build());
+    }
+}
